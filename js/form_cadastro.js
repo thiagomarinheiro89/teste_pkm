e805fa7c971c4cf42db0151f2202d9067a37b8d6
Thiago Marinheiro <marinheirocriacoes@gmail.com>
2023-01-10 17:21:19 -0300

    dados cartão ou conta

--- js/form_cadastro.js
+++ js/form_cadastro.js
@@ -2,4 +2,19 @@ $(document).ready(function(){
     $("#data_cad").val(data_atual());
     $("#cpf").inputmask({"mask": "999.999.999-99"});
     $("#telefone").inputmask({"mask": "(99) 99999-9999"});
+    $("#cartao").inputmask({"mask": "9999 9999 9999 9999"});
+    $("#venc_cartao").inputmask({"mask": "99/99"});
+    $("#cvv").inputmask({"mask": "999"});
+    
+    
 });
+
+function form_conta(){
+    $("#dados_conta").removeClass("d-none");
+    $("#dados_cartao").addClass("d-none");
+}
+
+function form_cartao(){
+    $("#dados_conta").addClass("d-none");
+    $("#dados_cartao").removeClass("d-none");
+}

b4d7ae22fb48299107f860ee0bd9a0aa5994eac2
Thiago Marinheiro <marinheirocriacoes@gmail.com>
2023-01-10 12:46:58 -0300

    mascara campos cpf e telefone

--- js/form_cadastro.js
+++ js/form_cadastro.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
     $("#data_cad").val(data_atual());
+    $("#cpf").inputmask({"mask": "999.999.999-99"});
+    $("#telefone").inputmask({"mask": "(99) 99999-9999"});
 });

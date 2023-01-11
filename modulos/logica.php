2e4fccb5409cb614bce07b7e34f59f4d4b9f9a52
Thiago Marinheiro <marinheirocriacoes@gmail.com>
2023-01-10 21:02:40 -0300

    resolução do execicio de logica

--- modulos/logica.php
+++ modulos/logica.php
@@ -1,2 +1,23 @@
+<?php
+    $sequencia = array(20,65,682,1050,1558,4032,5065,5095,6063,15000);
 
-log
+    $divisao_exata = verificacao($sequencia);
+
+    var_dump($divisao_exata);
+
+    function verificacao($sequencia){
+        $retorno = array();
+
+        for ($i=0; $i < count($sequencia)-1; $i++) { 
+            $resto = $sequencia[$i+1]-$sequencia[$i];
+
+            if (($resto % 4) == 0) {
+                $retorno[] = $resto;
+            };
+        }
+
+        return $retorno;
+    }
+
+
+?>
